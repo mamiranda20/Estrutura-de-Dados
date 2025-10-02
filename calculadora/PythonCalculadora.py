c595a466fed0be227ad5d11bc7293941e76ca5f6
mamiranda20 <123marcela@gmail.com>
2025-10-02 12:26:21 -0300

    Add files via upload

---
+++ calculadora/PythonCalculadora.py
@@ -0,0 +1,14 @@
+# Solicita os valores e converte de string para inteiro
+num1 = int(input("\nDigite o primeiro número: "))
+num2 = int(input("\nDigite o segundo número (diferente de zero): "))
+# Declara variáveis para armazenar os resultados
+soma = num1 + num2
+sub = num1 - num2
+mult = num1 * num2
+div = num1 / num2 # Em Python, a divisão já é float por padrão
+# Exibe os resultados das operações ao final
+print("\nResultados:")
+print("Soma:", soma)
+print("Subtração:", sub)
+print("Multiplicação:", mult)
+print("Divisão:", div)
